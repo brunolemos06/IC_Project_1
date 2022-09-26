b1bcca1dd1f7069217a3cfe68de1a04c6a3eb7f7
Bruno <blemos@ua.pt>
2022-09-26 20:06:19 +0100

    InsertProject

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # IC_Project_1
+
+# BRUNO LEMOS     | 98221
+# TIAGO MARQUES   |
+# JOÃO VIEGAS     |
---
+++ sndfile-example-src/build/CMakeFiles/wav_hist.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/bruno/Desktop/UA-LECI/4ano/IC/sndfile-example-bin/wav_hist"
+  "/home/bruno/Desktop/UA-LECI/4ano/IC/sndfile-example-bin/wav_hist.pdb"
+  "CMakeFiles/wav_hist.dir/wav_hist.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wav_hist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
